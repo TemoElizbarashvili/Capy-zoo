32ade80e77a95721947c0c26e91ee7160a9c420d
TemoElizbarashvili <115801621+TemoElizbarashvili@users.noreply.github.com>
2023-05-17 22:21:57 +0400

    add shopping cart page

---
+++ Kapizoo/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using Kapizoo.Models;
+using Kapizoo.Models.Repository;
+using Kapizoo.Models.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kapizoo.Controllers
+{
+    public class CartController : Controller
+    {
+        private IZooRepository ZooRepository;
+        private Cart cart;
+
+        public CartController(IZooRepository repo, Cart crtServices) 
+        {
+            ZooRepository = repo;
+            cart = crtServices;
+        }
+
+        public IActionResult Index()
+        {
+            return View(cart);
+        }
+    }
+}
